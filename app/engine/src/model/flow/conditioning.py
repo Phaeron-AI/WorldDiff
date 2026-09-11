0a198b21b92e793fcd89269ebaf0a0f32abc4d97
Aatti13 <aattreya.ks@gmail.com>
2026-09-11 22:32:56 +0530

    fix: added additinal tensor checks and update to mask tensor shape

--- app/engine/src/model/flow/conditioning.py
+++ app/engine/src/model/flow/conditioning.py
@@ -16,6 +16,22 @@ def apply_conditioning(
     
     mv = [B, V]
   """
-  mask = cond_mask[:, :, None, None]  # [B, V]
+  if zt.ndim != 5:
+    raise ValueError(f"Expected: [B, V, C, h, w]; Got: {tuple(zt.shape)}")
+
+  if zt.shape != z0.shape:
+    raise ValueError(
+      f"Both z0 and zt: [B, V, C, h, w]"
+      f"z0 shape: {tuple(z0.shape)}"
+      f"zt shape: {tuple(zt.shape)}" 
+    )
+
+  if cond_mask.ndim != 2:
+    raise ValueError(f"Expected: [B, V]; Got: {tuple(cond_mask.shape)}")
+
+  if cond_mask.shape[0] != z0.shape[0] or cond_mask.shape[1] != z0.shape[1]:
+    raise ValueError(f"Expected Shape: [B, V]; Got: {tuple(cond_mask.shape)}")
+  
+  mask = cond_mask[:, :, None, None, None]  # [B, V]
 
   return torch.where(mask, z0, zt)
