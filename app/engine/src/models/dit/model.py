588913445bed4b3238dfbc8b61dcaa526ccf5472
Aatti13 <aattreya.ks@gmail.com>
2026-09-16 20:21:10 +0530

    feat: added boilerplate code for MultiViewDiT class

---
+++ app/engine/src/models/dit/model.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+# Third Party Import(s)
+from torch import nn
+
+class MultiViewDiT(nn.Module):
+  def __init__(self) -> None:
+    super().__init__()
+    pass
+
+  def forward(self):
+    pass
